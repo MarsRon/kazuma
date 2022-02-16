a97dffd219048de788b18722227824dbf008716e
MarsRon <71264052+MarsRon@users.noreply.github.com>
2022-02-16 15:43:55 +0800

    Add compute time output to test.py

--- api/test.py
+++ api/test.py
@@ -1,4 +1,9 @@
 from kazuma import generate
+import time
 
 while True:
-  print(generate(input("=> ")))
+  start = time.perf_counter()
+  response = generate(input('=> '))
+  end = time.perf_counter()
+  print(response)
+  print(f'Compute time: {end - start:.3f}s')
