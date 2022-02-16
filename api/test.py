1acd22ff8905532633aa484bca23110ee01e728c
MarsRon <71264052+MarsRon@users.noreply.github.com>
2022-02-16 15:51:50 +0800

    Fix not accurate compute time

--- api/test.py
+++ api/test.py
@@ -2,8 +2,9 @@ from kazuma import generate
 import time
 
 while True:
+  message = input('=> ')
   start = time.perf_counter()
-  response = generate(input('=> '))
+  response = generate(message)
   end = time.perf_counter()
   print(response)
   print(f'Compute time: {end - start:.3f}s')
